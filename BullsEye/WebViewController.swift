98b65e88b619002b97a858bee87a50eae184d0e8
Deepanshu Gautam <gtmdeepanshu@gmail.com>
2018-10-23 11:26:07 +0900

    Bulls Eye beginner project done

---
+++ BullsEye/WebViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WebViewController.swift
+//  BullsEye
+//
+//  Created by deepanshugautam on 2018/10/22.
+//  Copyright © 2018 deepanshugautam. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
